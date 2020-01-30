ea968d64f6e65922d50be69b0926c289fe0f3415
EC2 Default User <ec2-user@ip-172-31-83-76.ec2.internal>
2020-01-30 14:14:54 +0000

    post fin7

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,11 @@ Rails.application.routes.draw do
   
   root to: 'tasks#index'
   
-  patch 'tasks/:id', to: 'tasks#update'
-  put 'tasks/:id', to: 'tasks#update'
-  get 'tasks/:id/edit', to: 'tasks#edit'
-
-  get 'users/new'
-  get 'users/create'
-  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  resources :tasks, only: [:index, :show, :new, :edit, :create, :destroy]
+  resources :tasks, only: [:index, :show, :new, :edit, :update, :create, :destroy]
   resources :users, only: [:new, :create]
 end
